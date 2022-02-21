c4616094665cd3656daf2a4a3004d6ac47720f56
Natalia Artini Ferrandin <79176834+Nataliaartini@users.noreply.github.com>
2022-02-21 10:04:05 -0300

    Update A001-insert.sql

--- sql/A001-insert.sql
+++ sql/A001-insert.sql
@@ -7,6 +7,7 @@ describe users;
 -- Inserir registros na base de dados
 insert into users 
 (first_name, last_name, email, password_hash) values
+("Luiz", "A.", "luiz@email.com", "a_hash"),
 ("Helena", "A.", "1@email.com", "3_hash"),
 ("Joana", "B.", "2@email.com", "4_hash"),
 ("Rosana", "C.", "3@email.com", "5_hash");
